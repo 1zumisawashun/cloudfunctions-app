ea1b23cd798220972520fa5d5995f5b538818d2d
izumisawashun <shun.1zumisawa@gmail.com>
2021-07-03 19:38:30 +0900

    put-request-in-firestore

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,12 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+//change region
+exports.changeRegion = functions
+  .region("asia-northeast1")
+  .https.onRequest((req, res) => {
+    console.log("hello tokyo reagion");
+  });
 // http request1
 exports.randomNumber = functions.https.onRequest((request, response) => {
   const number = Math.round(Math.random() * 100);
@@ -15,10 +23,46 @@ exports.sayHello = functions.https.onCall((data, context) => {
   // onCallを使う時はreturnで値を返す
 });
 
-exports.userDeleted = functions.auth.user().onCreate(user => {
-  console.log("user created", user.email, user.uid);
+exports.newUserSignup = functions.auth.user().onCreate(user => {
+  return admin
+    .firestore()
+    .collection("user")
+    .doc(user.uid)
+    .set({
+      email: user.email,
+      upvoteOn: []
+    });
 });
 
-exports.newUserSignup = functions.auth.user().onDelete(user => {
-  console.log("user deleted", user.email, user.uid);
+exports.userDeleted = functions.auth.user().onDelete(user => {
+  const doc = admin
+    .firestore()
+    .collection("user")
+    .doc(user.uid);
+  return doc.delete();
+});
+
+exports.addRequest = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    //ログインしていなかったら
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "only authenticated users can sdd requests"
+    );
+  }
+  if (data.text.length > 30) {
+    //30文字以上だったら
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "request must be no more than 30 characters long"
+    );
+  }
+  //onCallはreturnをする
+  return admin
+    .firestore()
+    .collection("request")
+    .add({
+      text: data.text,
+      upvotes: 0
+    });
 });
--- plugins/app.js
+++ plugins/app.js
@@ -1,5 +1,8 @@
+import firebase from "~/plugins/firebase";
+
 const requestModal = document.querySelector(".new-request");
 const requestLink = document.querySelector(".add-request");
+const requestForm = document.querySelector(".new-request form");
 
 requestLink.addEventListener("click", () => {
   requestModal.classList.add("open");
@@ -11,4 +14,24 @@ requestModal.addEventListener("click", e => {
     console.log(e.target);
     requestModal.classList.remove("open");
   }
+});
+
+requestForm.addEventListener("submit", e => {
+  e.preventDefault();
+  console.log("requestをしています！");
+  const addRequest = firebase.functions().httpsCallable("addRequest");
+  addRequest({
+    text: requestForm.request.value
+  })
+    .then(() => {
+      requestForm.reset();
+      console.log("ここまで来ている？");
+      requestModal.classList.remove("open");
+      requestForm.querySelector(".error").textContent = "";
+    })
+    .catch(error => {
+      alert("error!");
+      requestForm.querySelector(".error").textContent = error.message;
+    });
+  console.log("一旦ここまで確認できる？");
 });
